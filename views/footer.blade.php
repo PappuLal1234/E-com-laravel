fb2d2c133e773a422548480244d527e918f6de57
Pappu Lal <73828213+PappuLal1234@users.noreply.github.com>
2021-04-15 20:09:57 +0530

    Add files via upload

---
+++ views/footer.blade.php
@@ -0,0 +1,16 @@
+<div style="clear:both;" class="panel panel-default">
+  
+
+
+<div class="panel-body">
+ 
+   
+<p style="color:blue;text-align:center;">Copyright &copy; 2021 PappuPajer</p>
+</div>
+<div class="panel-footer">
+  
+ <p style="color:blue;text-align:center;"> Designed by : @Partusha</p>
+</div>
+</div> 
+
+  
